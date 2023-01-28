06b8ebb4f3accae3af666940a6902e7027772e8d
Thiago Mota <th.araujomota@gmail.com>
2023-01-28 18:44:01 -0300

    refac: removes space after ingredient name

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany({
   nome: { $ne: "McChicken" } },
-  { $addToSet: { ingredientes: "Ketchup " } });
+  { $addToSet: { ingredientes: "ketchup" } });
 
 db.produtos.find({}, {
   _id: 0,
